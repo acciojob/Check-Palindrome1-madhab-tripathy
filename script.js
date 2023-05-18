2a05d2872cb9f74ce5312ccc200171b7002c6f85
Madhab Tripathy <66151264+madhab-tripathy@users.noreply.github.com>
2023-05-18 18:45:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // complete the given function
 
 function palindrome(str){
-
+	let reverseWord = str.split(" ").map((e) => {return e.trim();}).reverse().join("");
+    return (str === reverseWord)? true : false;
 }
 module.exports = palindrome
